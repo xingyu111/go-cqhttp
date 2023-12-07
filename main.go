26db4123a0fff72f5f70d7e6b4ae52ea94c81ab7
xingyu111 <153156338+xingyu111@users.noreply.github.com>
2023-12-07 12:43:03 +0800

    Update main.go

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 	_ "github.com/Mrs4s/go-cqhttp/db/leveldb"   // leveldb 数据库支持
 	_ "github.com/Mrs4s/go-cqhttp/modules/silk" // silk编码模块
-	// 其他模块
+	// 其他模块11
 	// _ "github.com/Mrs4s/go-cqhttp/db/sqlite3"   // sqlite3 数据库支持
 	// _ "github.com/Mrs4s/go-cqhttp/db/mongodb"    // mongodb 数据库支持
 	// _ "github.com/Mrs4s/go-cqhttp/modules/pprof" // pprof 性能分析
